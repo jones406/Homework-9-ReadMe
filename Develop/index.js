c09e7bc1e93257db6c9d201154ccf2b65767b507
brooke <brookejones406@gmail.com>
2021-10-15 08:36:45 -0500

    working on functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,77 +1,38 @@
-// TODO: Include packages needed for this application
+//Tell JS which files or packages to get
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input. Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//Create an array of questions for user input.
 const questions = [
-    {
-        name: "project",
-        message: "What is your project title?",
-        type: "input",
-    },
-    {
-        name: "description",
-        message: "Describe your project in a few short sentences.",
-        type: "input",
-    },
-    {
-        name: "toc",
-        message: "Table of Contents.",
-        type: "input",
-    },
-    {
-        name: "install",
-        message: "How do you install your project?",
-        type: "input",
-    },
-    {
-        name: "usage",
-        message: "List usage steps.",
-        type: "input"
-    },
-    {
-        name: "credits",
-        message: "List contributors/collaborators, third-party assets that require attributes, or tutorials you used.",
-        type: "input",
-    },
-    {
-        name: "license",
-        message: "How old are you?",
-        type: "number",
-    },
-    {
-        name: "contribute",
-        message: "Describe how others can contribute.",
-        type: "input",
-    },
-    {
-        name: "test",
-        message: "Provide examples on how to run tests here.",
-        type: "input",
-    },
-    {
-        name: "questions",
-        message: "Where should questions be directed?",
-        type: "number",
-    },
-];
+    {name: "title", message: "Project title:", type: "input"},
+    {name: "description", message: "Short project description:", type: "input"},
+    {name: "install", message: "How do you install your project?", type: "input"},
+    {name: "usage", message: "List usage steps.", type: "input"},
+    {name: "credits", message: "List contributors and references:", type: "input"},
+    {name: "license", message: "License:", type: "checkbox", choices:["MIT", "GPLv2","Apache", "Other"]},
+    {name: "contribute", message: "How to contribute:", type: "input",},
+    {name: "test", message: "How to run tests:", type: "input"},
+    {name: "questions", message: "Where should questions be directed?", type: "input"}]
 
-// TODO: Create a function to write README file
-function writeToFile('README.md', process.argv[2],) {
-    
-}
+// Function to write README file
+// function writeToFile(filename, data) { };
+fs.writeFile('README.md', generateMarkdown(data), err => {
+    err ? console.log(err) : console.log('Success!')
+});
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(function(response) {  //response is the name I'm giving to the object created by user answers
-        console.log(response.project);
-    })   
-    .catch(function(err) {
-        console.log(err);
-    })
+        .then(function (response) {  //response is the name I'm giving to the object created by user answers
+            console.log(response.project);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
 }
 
 // Function call to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,48 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in. If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (data.license == "MIT") {
+    return 
+  }
+  if (data.license == "GPLv2") {
+    return 
+  }
+  if (data.license == "Apache") {
+    return 
+  }
+  else {
+    return ``
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (data.license) {
+    return 
+  }
+  else {
+    return ``
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// TODO: Create a function that returns the license section of README. If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (data.license = false) {
+    data.license = "";
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## ${data.description}
+  ## ${data.install
+  ## ${data.usage}
+  ## ${data.credits}
+  ## ${data.license}
+  ## ${data.contribute}
+  ## ${data.test}
+  ## ${data.questions}
 `;
 }
 
